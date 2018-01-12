f57a106e25b9b5ba7e1f4a5676885056eda18f22
braidoj <jorgebraido@gmail.com>
2018-01-12 13:56:29 -0200

    Initial commit

---
+++ app/src/main/java/com/example/sony/acc/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.sony.acc;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    //Cria um objeto para o sensor do acelerômetro
+    private Sensor meuAcelerômetro;
+
+    // Criado para acessar o acelerômetro
+    private SensorManager gerenciamentoAcelerômetro;
+
+    // Cria um objeto para o TextView
+    private TextView visorDoTextoX, visorDoTextoY,visorDoTextoZ;
+
+    // Cria a tela inicial do applicativo
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Cria o gerenciador do acelerômetro//
+        gerenciamentoAcelerômetro = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        //Cria o acelerômetro
+        meuAcelerômetro = gerenciamentoAcelerômetro.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Registra o sensor
+        gerenciamentoAcelerômetro.registerListener(this,meuAcelerômetro,SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Atribui as TextViews a cada eixo
+        visorDoTextoX = (TextView) findViewById(R.id.visorDoTextoX);
+        visorDoTextoY = (TextView)findViewById(R.id.visorDoTextoY);
+        visorDoTextoZ = (TextView)findViewById(R.id.visorDoTextoZ);
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+        //método não utilizado - chamado quando a precisão do acelerômetro é modificada
+
+    }
+
+    @Override
+    //Faz a leitura da aceleração em cada eixo
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        visorDoTextoX.setText("X= " + sensorEvent.values[0]);
+        visorDoTextoY.setText("Y= " + sensorEvent.values[1]);
+        visorDoTextoZ.setText("Z= " + sensorEvent.values[2]);
+
+    }
+
+
+}
